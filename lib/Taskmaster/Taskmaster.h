871d47e5e038adeefb5db4f7ba095b14fe7e48c0
Justin Narayan <justin1narayan@gmail.com>
2025-11-06 15:39:57 -0500

    Implement Taskmaster which manages Comms and Controller objects

---
+++ lib/Taskmaster/Taskmaster.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <CommsInterface.h>
+#include <Controller.h>
+
+/*****************************************************
+ *                 COMPILER UTILITIES                *
+ *****************************************************/
+#define LOOP_CONTROLLER_IDX(controllerVar)                                \
+    for (size_t controller_idx = 0; controller_idx < numControllers; ++controller_idx)
+		
+#define CONTROLLERS_NUM_ELEMENTS(c) (size_t)(sizeof(c)/sizeof(c[0]))
+#define TASKMASTER_DECLARE(name, comms, controllers) \
+	Taskmaster name(comms, controllers, CONTROLLERS_NUM_ELEMENTS(controllers));
+
+/**
+ * @brief A Taskmaster object arbitrates between the CommsInterface and all Controller objects.
+ * Every loop, the Taskmaster:
+ * - Reads the CommsInterface for any Message object
+ * - If a Message is received, it will disseminate it to all Controller objects receiving that 
+ * type of Message
+ * - Allow all Controller objects to process
+ * - Checks for all Controllers with Message objects to send and preaches them
+ * 
+ * @tparam numControllers 
+ */
+class Taskmaster 
+{
+private:
+	CommsInterface* comms;
+	ControllerGeneric** controllers;
+	size_t numControllers;
+
+	bool poll(Message* message);
+	void disseminate(Message* message);
+	void process(void);
+	void preach(void);
+public:
+	Taskmaster(
+		CommsInterface* comms, ControllerGeneric* controllers[], size_t numControllers
+	) : comms(comms), controllers(controllers), numControllers(numControllers) {};
+
+	void execute(void);
+};
